85a961bdeecccfb1767bc067c337c5ee0a6258ce
user <menaveenpal@gmail.com>
2022-07-19 23:52:10 +0530

    arrange in systematic ways

---
+++ entity/entity.go
@@ -0,0 +1,12 @@
+package entity
+
+type UserProfile struct {
+	FullName    string `json:"fullname"`
+	EmailId     string `json:"emailid"`
+	PhoneNumber string `json:"phonenumber"`
+}
+
+type OTP struct {
+	PhoneNumber string `json:"phonenumber"`
+	Otp         string `json:"otp"`
+}
