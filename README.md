9e8ee42d934606cd6e822b7b33408b0f3b2b67ae
amren1254 <y.amren00@gmail.com>
2022-07-19 18:31:40 +0530

    Amrendra:feat/0:added boilerplate code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # login-service
+
+This is a login microservice
+creating this for learning purpose
+
+Will use
+    gin-gonic
+    psql
---
+++ user.sql
@@ -0,0 +1,17 @@
+-- postgres
+-- \l+ -> to show show databases
+-- sudo -u postgres psql postgres
+-- ALTER USER postgres PASSWORD 'root';
+CREATE DATABASE userlogin;
+
+ALTER TABLE userprofile ALTER COLUMN phonenumber TYPE varchar(14);
+ALTER TABLE userprofile ADD isVerified boolean DEFAULT false;
+
+UPDATE userprofile SET isUserVerified=true WHERE phonenumber='8896726484';
+
+
+CREATE TABLE userprofile (fullname varchar(100), emailid varchar(100), phonenumber char(13) NOT NULL,PRIMARY KEY (phonenumber));
+
+
+INSERT INTO userprofile(fullname, emailid, phonenumber) VALUES('amrendra','yamren00@gmail.com',8896726484);
+INSERT INTO userprofile(fullname, emailid, phonenumber) VALUES('naveen','menaveenpal@gmail.com',88967264840);
